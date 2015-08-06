a318b00424ff6dd4b86f81f5f370ee37287c5524
Rich Fletcher <rich.p.fletcher@gmail.com>
2015-08-06 13:10:31 -0400

    Updated LAMP script with GitHub content

--- scripts/centos-lamp.sh
+++ scripts/centos-lamp.sh
@@ -38,5 +38,8 @@ service mysqld start
 mysql -u root -e "SHOW DATABASES";
 
 #Download starter content
+cd /vagrant
+sudo -u vagrant wget -q https://raw.githubusercontent.com/rfletche847/vagrant/master/files/index.html
+sudo -u vagrant wget -q https://raw.githubusercontent.com/rfletche847/vagrant/master/files/info.php
 
 service httpd restart
